3f1dc1adfcc87fb735f49c21b41c599e363e0fe3
Serkan Çamur <serkancamur@gmail.com>
2020-10-28 16:41:08 +0300

    ProjectInit

---
+++ ExpressionToTSQL/ExpressionToTSQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpressionToTSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
